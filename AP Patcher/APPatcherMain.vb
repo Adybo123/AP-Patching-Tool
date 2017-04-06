e5f856e279d9e4921a34a711d3aca9deaefaef02
Adam <adam@adybo.co.uk>
2017-04-06 15:55:23 +0100

    Early, broken, non-working push
    Getting there

--- AP Patcher/APPatcherMain.vb
+++ AP Patcher/APPatcherMain.vb
@@ -1,9 +1,107 @@
-﻿Module APPatcherMain
+﻿Imports Ionic.Zip
+Module APPatcherMain
+
+    'Declarations
+    Dim RepoURL As String = "http://www.adybo.co.uk/files/myfpacks/"
+    Dim CurrentDirectory As String = System.IO.Directory.GetCurrentDirectory
+    Dim GameDirectory As String = ""
+    Dim PrefLoadFail As Boolean = False
 
     Sub Main()
-        Console.WriteLine("AP Patching Tool")
+        LoadGameDirectoryPreference()
+        If PrefLoadFail = False Then
+            BackToMainMenu()
+        End If
+    End Sub
+
+    Sub DrawLogo()
+        Console.WriteLine("=======================")
+        Console.WriteLine("AP Patcher package tool")
+        Console.WriteLine("Created by Adam Soutar")
+        Console.WriteLine("=======================")
         Console.WriteLine("")
-        Console.WriteLine(TakeConsoleInput())
+    End Sub
+
+    Sub LoadGameDirectoryPreference()
+        Console.Clear()
+        DrawLogo()
+        Try
+            Dim SysRdr As New System.IO.StreamReader(CurrentDirectory & "\GameDirectory.txt")
+            GameDirectory = SysRdr.ReadLine
+            SysRdr.Close()
+        Catch ex As Exception
+            Console.WriteLine("Loading preferences failed!")
+            Console.WriteLine("Couldn't find this file: " & CurrentDirectory & "\GameDirectory.txt")
+            Console.WriteLine("")
+            Console.WriteLine("The application can't start without this file.")
+            Console.WriteLine("")
+            Console.Write("Press any key to quit.")
+            PrefLoadFail = True
+            Dim QuitResponse As String = Console.ReadLine
+        End Try
+    End Sub
+
+    Sub BackToMainMenu()
+        Console.Clear()
+        DrawLogo()
+        Console.WriteLine("Automatic patching tool for automated packages.")
+        Console.WriteLine("")
+        Console.WriteLine("Please enter the desired Pack ID:")
+        Console.Write(">")
+        Dim PackIdToFetch As String = Console.ReadLine
+        PatchFromID(PackIdToFetch)
+    End Sub
+
+    Sub PatchFromID(PackID)
+        Console.WriteLine("")
+        Console.WriteLine("Fetching pack " & PackID & "...")
+
+        'Attempt pack download
+        Dim LocalPackPath As String = System.IO.Directory.GetCurrentDirectory & "\" & PackID & ".zip"
+        Try
+            My.Computer.Clipboard.SetText(RepoURL & PackID & ".zip")
+            My.Computer.Network.DownloadFile(RepoURL & PackID & ".zip", LocalPackPath, "", "", True, 10000, True)
+            Console.WriteLine("Asking repo for response... OK!")
+            Console.WriteLine("Checking pack exists... OK!")
+            Console.WriteLine("Patch downloaded successfully.")
+        Catch ex As Exception
+            Console.WriteLine("Asking repo for response... FAIL!")
+            Console.WriteLine("Pack ID might be incorrect, repo URI may be invalid or not responding correctly.")
+        End Try
+
+        'Attempt zip file extraction
+        Console.WriteLine("Attempting to extract compressed pack...")
+        Try
+            Dim ZipToUnpack As String = LocalPackPath
+            Dim UnpackDirectory As String = GameDirectory & "\"
+            Using zip1 As ZipFile = ZipFile.Read(ZipToUnpack)
+                Dim e As ZipEntry
+                Dim TotalItems As Integer = 0
+                For Each x In zip1
+                    TotalItems = TotalItems + 1
+                Next
+                Console.WriteLine("File scan complete. Compressed file contains " & TotalItems & " textures.")
+                Dim CurrentItem As Integer = 1
+                For Each e In zip1
+                    Console.WriteLine("Extracting item " & CurrentItem & " of " & TotalItems)
+                    e.Extract(UnpackDirectory, ExtractExistingFileAction.OverwriteSilently)
+                    CurrentItem = CurrentItem + 1
+                Next
+            End Using
+            Console.WriteLine("")
+            Console.WriteLine("Pack extracted, game patched.")
+        Catch ex As Exception
+            Console.WriteLine("")
+            Console.WriteLine("Failed to extract the patch! The Pack ID may have been invalid or the repo might not be responding properly.")
+        End Try
+        Console.WriteLine("")
+        Console.WriteLine("Type 'quit' to close the application, type 'menu' to return to the menu.")
+        Console.Write(">")
+        Dim OptionResponse As String = Console.ReadLine
+        If OptionResponse.ToLower() = "quit" Then
+            Exit Sub
+        End If
+        BackToMainMenu()
     End Sub
 
     Function TakeConsoleInput()
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Automated patching tool for downloading and extracting compressed packages
 
 Usage information and documentation coming soon.
+
+AP Patching Tool created by Adam Soutar
+The tool uses Ionic.Zip libraries created by DotNetZip
